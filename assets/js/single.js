c93baf125b876eabf0091ff25a97cbe880bb160a
Meghan Bock <meghan.bock@gmail.com>
2021-06-24 10:47:58 -0500

    updated files related to api #2

--- assets/js/single.js
+++ assets/js/single.js
@@ -11,22 +11,12 @@ var recipeObj = {
   friday: [],
   saturday: [],
 };
-var drinkObj = {
-  sunday: [],
-  monday: [],
-  tuesday: [],
-  wednesday: [],
-  thursday: [],
-  friday: [],
-  saturday: [],
-};
-console.log(recipeObj);
+var queryString = document.location.search;
 var singleRecipe = {};
 
 $("#currentDay").text(currentDate);
 
 var getRecipeName = function () {
-  var queryString = document.location.search;
   var recipeId = queryString.split("=")[1];
   recipeId = recipeId.split("&")[0];
   console.log("recipe ID: " + recipeId);
@@ -44,10 +34,7 @@ var getRecipeName = function () {
 }
 
 var getRecipe = function (recipeId) {
-  var queryString = document.location.search;
-  if (
-    queryString.includes("drinkId")
-  ) {
+  if (queryString.includes("drinkId")) {
     var apiKey = "1";
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + recipeId;
     console.log(apiUrl)
@@ -69,9 +56,7 @@ var getRecipe = function (recipeId) {
       }
     });
   }
-  else if (
-    queryString.includes("recipeId")
-  ) {
+  else if (queryString.includes("recipeId")) {
     var apiId = "91e2da9b";
     var apiKey = "d56ab70c91a98e462f1a95f47179d8a1";
 
@@ -105,40 +90,28 @@ var getRecipe = function (recipeId) {
 };
 
 var displayRecipe = function (recipe) {
-  console.log("displayRecipe")
   var recipeName = "";
-  var queryString = document.location.search;
-  var imageUrl="";
-  var ingredientsArr=[];
-  var instructions="";
-  if (
-    queryString.includes("drinkId")
-  ) {
+  var imageUrl = "";
+  var ingredientsArr = [];
+  var instructions = "";
+  if (queryString.includes("drinkId")) {
     recipeName = recipe.strDrink;
-    console.log(recipeName);
-    imageUrl= recipe.strDrinkThumb;
+    imageUrl = recipe.strDrinkThumb;
     instructions= recipe.strInstructions;
-    console.log(instructions);
-    for (
-      var i= 0; i < 15; i++
-    ) {
-      console.log("loop started")
+    for (var i= 0; i < 15; i++) {
       var ingredient= recipe["strIngredient" + (i + 1)];
-      if (
-        ingredient
-      ) {
-        console.log(ingredient);
+      if (ingredient) {
         ingredientsArr.push(ingredient);
       }
     }
-    console.log(ingredientsArr);
   }
-  else if (
-    queryString.includes("recipeId")
-  ) {
+  else if (queryString.includes("recipeId")) {
     recipeName = recipe.label;
-    imageUrl= recipe.image;
-    ingredientsArr= recipe.ingredients;
+    imageUrl = recipe.image;
+    for(var i = 0; i < recipe.ingredientLines.length; ++i) {
+      var ingredient = recipe.ingredientLines[i];
+      ingredientsArr.push(ingredient);
+    }
   }
   
   recipeNameEl.textContent = recipeName;
@@ -147,6 +120,7 @@ var displayRecipe = function (recipe) {
   }
 
   var recipeImg = document.createElement("img");
+  recipeImg.classList = "recipe-img";
   recipeImg.setAttribute("src", imageUrl);
   recipeContainerEl.appendChild(recipeImg);
 
@@ -161,13 +135,18 @@ var displayRecipe = function (recipe) {
 
   recipeContainerEl.appendChild(ingredientsList);
 
-  var instructionsButton = document.createElement("a");
-  //instructionsButton.setAttribute("href", recipe.url);
-  //instructionsButton.setAttribute("target", "_blank");
-  instructionsButton.textContent = "Preparation Instructions";
-
-  recipeContainerEl.appendChild(instructionsButton);
-  console.log("instructions")
+  if (queryString.includes("drinkId")) {
+    var instructionsEl = document.createElement("p");
+    instructionsEl.textContent = "Preparation Instructions: " + recipe.strInstructions;
+    recipeContainerEl.appendChild(instructionsEl);
+  }
+  else if (queryString.includes("recipeId")) {
+    var instructionsButton = document.createElement("a");
+    instructionsButton.setAttribute("href", recipe.url);
+    instructionsButton.setAttribute("target", "_blank");
+    instructionsButton.textContent = "Preparation Instructions";
+    recipeContainerEl.appendChild(instructionsButton);
+  }
 };
 
 var loadRecipeLocalStorage = function () {
@@ -194,33 +173,56 @@ var saveRecipeLocalStorage = function () {
   localStorage.setItem("recipes", JSON.stringify(recipeObj));
 }
 
-$("#modal-save").click(function () {
-  var day = $("#day-input").val().toLowerCase();
-  var mealType = $("#meal-input").val().toLowerCase();
-  var recipeName = singleRecipe.label;
-  var recipeUrl = singleRecipe.url;
-  console.log("recipe URL: " + recipeUrl);
-  console.log("day: " + day);
-  console.log("recipe object: " + recipeObj);
-  console.log("day object: " + recipeObj[day]);
+$("#modal-save, #modal-save-2").click(function () {
+  var recipeName = "";
+  var day = "";
+  var recipeType = "";
+  var mealType = "";
+  var recipeUrl = "";
 
-  if (!day || !mealType) {
-    alert("You must provide input for both Day of the Week and Meal Type");
+  if (queryString.includes("drinkId")) {
+    day = $("#day-input-2").val();
+    if(!day) {
+    } else {
+      recipeName = singleRecipe.strDrink;
+      day = day.toLowerCase();
+      recipeType = "drink";
+    }
   }
-  else {
+  else if (queryString.includes("recipeId")) {
+    recipeName = singleRecipe.label;
+    day = $("#day-input").val();
+    mealType = $("#meal-input").val();
+
+    if (!day && !mealType) {
+      alert("You must provide input for both Day of the Week and Meal Type");
+    }
+    else {
+      day = day.toLowerCase();
+      mealType = mealType.toLowerCase();
+      recipeUrl = singleRecipe.url;
+      recipeType = "food";
+    }
+  }
+
+  console.log(recipeObj[day]);
 
     // close modal
-    $("#a").fadeOut();
-    $("#b").fadeOut();
+    $("#drink-modal").fadeOut();
+    $("#food-modal").fadeOut();
+
     // save in tasks array
     recipeObj[day].push({
       name: recipeName,
       mealType: mealType,
       url: recipeUrl,
+      recipeType: recipeType,
     });
 
     saveRecipeLocalStorage();
-  }
+    $("#day-input-2").val = "";
+    $("#day-input").val = "";
+    $("#meal-input").val = "";
 });
 
 loadRecipeLocalStorage();
@@ -229,11 +231,20 @@ getRecipeName();
 
 // opem and close modal
 $("#open").click(function () {
-  $("#a").css("display", "block");
-  $("#b").css("display", "block");
+  if (queryString.includes("recipeId")) {
+  $("#food-modal").css("display", "block");
+  }
+  else if(queryString.includes("drinkId")) {
+    $("#drink-modal").css("display", "block");
+  }
 });
 
 $("#modal-close").click(function () {
-  $("#a").fadeOut();
-  $("#b").fadeOut();
+  $("#drink-modal").fadeOut();
+  $("#food-modal").fadeOut();
+});
+
+$("#modal-close-2").click(function () {
+  $("#drink-modal").fadeOut();
+  $("#food-modal").fadeOut();
 });
